c67c51f544ae58df1cb67c8daff7a03af3f5ed9a
Asher James <asherjames.nz@gmail.com>
2016-06-02 16:41:01 +1200

    Add imgReader.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const async = require('async');
 const express = require('express');
+const _ = require('lodash');
 
 const config = require('../conf/config.json');
 const scanner = require('./dirScanner');
@@ -8,6 +9,7 @@ const app = express();
 
 
 app.get('/seikkor/photos', (req, res) => {
+    let imageInfos = {};
     async.series([
             (cb) => {scanner.getAllFilenames(config.fullsizeImagePath, cb)},
             (cb) => {scanner.getAllFilenames(config.thumbnailImagePath, cb)}
@@ -15,13 +17,19 @@ app.get('/seikkor/photos', (req, res) => {
         (err, results) => {
             compareDirs(results[0], results[1])
     });
+    res.json(imageInfos);
 });
 
 app.listen(3000, () => {
 	console.log("Seikkor API listening on 3000...");
 });
 
-const compareDirs = (full, thumb) => {
-    console.log(full);
-    console.log(thumb);
-};
+function compareDirs(fulls, thumbs) {
+    let noThumbs = fulls.filter((item) => {
+        return !_.includes(thumbs, item);
+    });
+
+    if(noThumbs.length) {
+        
+    }
+}
