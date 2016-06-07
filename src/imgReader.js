2e146fc9bb956dcf9cde42d100bb791c5e79e699
Asher James <asherjames.nz@gmail.com>
2016-06-07 15:59:58 +1200

    Make imgReader get both fullsize and thumbnail dimensions

--- src/imgReader.js
+++ src/imgReader.js
@@ -1,5 +1,35 @@
+const async = 
+const path = require('path');
 const sizeOf = require('image-size');
 
-module.exports.getImageInfo = (path, cb) => {
-    
+module.exports.getImageInfo = (fullPath, thumbPath, cb) => {
+    async.series([
+        (asyncCb) => {
+            getImageSize(thumbPath, asyncCb);           
+        },
+        (asyncCb) => {
+            getImageSize(thumbPath, asyncCb);                       
+        }
+    ], (err, results) => {
+        let imgName = path.basename(fullPath);
+        let imgInfo = {
+            src: imgName,
+            thumbnail: imgName,
+            w: results[0].width,
+            h: results[0].height,
+            thumbW: results[1].width,
+            thumbH: results[1].height
+        };
+        cb(imgInfo);
+    });
+}
+
+function getImageSize(path, asyncCb) {
+    sizeOf(path, (err, dim) => {
+        if(err) {
+            console.error("Error reading image information", err);
+            return;
+        }
+        asyncCb(dim);
+    });
 }
