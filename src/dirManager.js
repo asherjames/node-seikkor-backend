09f5570acb6e6bdf8d0241720ec615bf20806038
Asher James <asherjames.nz@gmail.com>
2016-06-10 14:55:30 +1200

    Create dirManager

---
+++ src/dirManager.js
@@ -0,0 +1,49 @@
+const async = require('async');
+const _ = require('lodash');
+const Thumbnail = require('thumbnail');
+
+const maxSize = require('../conf/config.json').maxThumbnailSize;
+const fullPath = require('../conf/config.json').fullsizeImagePath;
+const thumbPath = require('../conf/config.json').thumbnailImagePath;
+const reader = require('./dirReader');
+
+
+function compareDirs(fulls, thumbs) {
+    let noThumbs = fulls.filter((item) => {
+        return !_.includes(thumbs, item);
+    });
+
+    if(noThumbs.length) {
+        updateThumbs(noThumbs);
+    }
+}
+
+function updateThumbs(noThumbs) {
+    if(noThumbs.length) {
+        async.each(noThumbs, (item, done) => {
+            reader.getImageSize(`${fullPath}/${item}`, (err, dim) => {
+                if(err) {
+                    console.error("Error getting image size", err);
+                }
+                generateThumb(item, dim);
+            }) 
+        });
+    }    
+}
+
+function generateThumb(img, dim) {
+    let thumb = new Thumbnail(fullPath, thumbPath);
+    let width = null;
+    let height = null;
+    if (dim.width > dim.height) {
+        width = maxSize;
+    } else {
+        height = maxSize;
+    }
+    thumb.ensureThumbnail(img, width, height, (err, thumbName) => {
+        if (err) {
+            console.error("Error creating thumbnail", err);
+        }
+        console.log(`Created thumbnail ${thumbName}`);
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 const async = require('async');
 const express = require('express');
-const _ = require('lodash');
-const Thumbnail = require('thumbnail');
 
 const fullPath = require('../conf/config.json').fullsizeImagePath;
 const thumbPath = require('../conf/config.json').thumbnailImagePath;
-const maxSize = require('../conf/config.json').maxThumbnailSize;
 const scanner = require('./dirScanner');
 
 const app = express();
 
+const logger = (req, res, next) => {
+    console.log("Request received...");
+    next();
+};
+
+app.use(logger);
 
 app.get('/seikkor/photos', (req, res) => {
     let imageInfos = {};
@@ -26,23 +29,3 @@ app.get('/seikkor/photos', (req, res) => {
 app.listen(3000, () => {
 	console.log("Seikkor API listening on 3000...");
 });
-
-function compareDirs(fulls, thumbs) {
-    let noThumbs = fulls.filter((item) => {
-        return !_.includes(thumbs, item);
-    });
-
-    if(noThumbs.length) {
-        updateThumbs(noThumbs);
-    }
-}
-
-function updateThumbs(noThumbs) {
-    let thumb = new Thumbnail(fullPath, thumbPath);
-
-    if(noThumbs.length) {
-        async.each(noThumbs, (item, done) => {
-            
-        });
-    }    
-}
